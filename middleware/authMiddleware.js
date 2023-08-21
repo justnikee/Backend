15c36c6a1d6518d8ae5ddf0aa83d5a45f7414f7d
justNikee <hk5561111212@gmail.com>
2023-08-22 00:27:37 +0530

    middleware fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,43 +1,39 @@
-const JWT = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { Users } = require('../model/users');
 
-
 const requireSignIn = (req, res, next) => {
-    try {
-        const decode = JWT.verify(
-            req.headers.authorization,
-            process.env.JWT_SECRET
-        );
-        req.user = decode;
-        next();
-    } catch (error) {
-        console.log(error);
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ success: false, message: 'Authorization token is missing' });
     }
-}
 
-
-const isAdmin = async(req, res, next) => {
-   try {
-    const user = await Users.findById(req.user._id);
-    console.log(user.role)
-    if(user.role != 1){
-        return res.status(401).send({
-            success : false,
-            message: 'Unauthorize Access!'
-        });
-    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
     next();
-   } catch (error) {
-    console.log(error)
-    res.status(401).send({
-        success: false,
-        error,
-        message: 'isAdmin middleware error!'
-    })
-   }
-}
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ success: false, message: 'Invalid token' });
+  }
+};
+
+const isAdmin = async (req, res, next) => {
+  try {
+    const user = await Users.findById(req.user._id);
+    console.log(user.role);
+
+    if (user.role !== 1) {
+      return res.status(401).json({ success: false, message: 'Unauthorized Access!' });
+    }
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ success: false, error, message: 'isAdmin middleware error' });
+  }
+};
 
 module.exports = {
-    requireSignIn,
-    isAdmin
-}
+  requireSignIn,
+  isAdmin
+};
